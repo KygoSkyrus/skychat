4854d1380136e62f87a4792f90990cec7c303947
Dheeraj Gupta <dheerajg@digitizeco.in>
2024-07-15 12:18:02 +0530

    build deployed

--- client/src/firebaseConfig.js
+++ client/src/firebaseConfig.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBUwUDMBV60Bo_EAUcXuPV4Xk7SkVmnVMo",
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "skyrus-3416b.firebaseapp.com",
   databaseURL: "https://skyrus-3416b-default-rtdb.firebaseio.com",
   projectId: "skyrus-3416b",
   storageBucket: "skyrus-3416b.appspot.com",
-  messagingSenderId: "1014608195968",
-  appId: "1:1014608195968:web:19d0c28fa58278de6dd165",
-  measurementId: "G-KYYRGB8KNR"
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_SENDER_ID,
+  appId:process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
